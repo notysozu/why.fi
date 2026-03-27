141602cc217a3e3a5e98a5e7ee2c318dab2c91f5
notysozu <lifegamer2050@gmail.com>
2026-03-28 02:21:50 +0530

    feat(install.ps1): add Chocolatey/Winget detection

--- install.ps1
+++ install.ps1
@@ -1,3 +1,6 @@
 #Requires -Version 5.1
 $ErrorActionPreference = 'Stop'
 Write-Host "Detecting Windows Version: $([System.Environment]::OSVersion.Version)"
+$pkgMgr = "none"
+if (Get-Command choco -ErrorAction SilentlyContinue) { $pkgMgr = "choco" }
+elseif (Get-Command winget -ErrorAction SilentlyContinue) { $pkgMgr = "winget" }
